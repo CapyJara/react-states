d438eb24f2bf29a51b2019a0eb77e1b4f0fe9ebd
UncleJerry <jared.myhrberg@gmail.com>
2019-05-14 13:50:30 -0700

    random color workign

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+const chance = require('chance').Chance();
 
-export default function RandomColor() {
+export default class RandomColor extends PureComponent {
+  state = {
+    color: '',
+    trigger: new Date(),
+    img: ''
+  }
 
+  randomColor = () => {
+    const color = chance.pickone(['red', 'blue', 'green', 'yellow', 'orange', 'purple']);
+    return color;
+  }
 
-  return ()
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState({ color: this.randomColor(), trigger: new Date(), img: '' });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.color === this.state.color) {
+      this.setState({ img: 'https://thumbs.gfycat.com/DemandingWelltodoGallinule-poster.jpg' });
+    }
+  }
+
+  render() {
+    const { color, img } = this.state;
+    const styles = {
+      background: img ? `center / contain url(${img}` : color,
+      width: '50em',
+      height: '50em'
+    };
+    return <div style={styles}></div>;
+  }
 }
