c378dc1b68b97eecc928e72007f4a53eb462326c
UncleJerry <jared.myhrberg@gmail.com>
2019-05-15 09:54:14 -0700

    in progress

---
+++ src/components/ColorDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ColorDisplay';
+
+function ColorDisplay({ colo }) {
+  return (
+    <div className=
+  )
+}
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ColorDisplay';
+
+function ColorPicker({ selectColorHandler }) {
+  return (
+    <section className={styles.ColorPicker}>
+      <button className={styles.red} onClick={() => selectColorHandler('red')}>Red</button>
+      <button className={styles.yellow} onClick={() => selectColorHandler('yellow')}>Yellow</button>
+      <button className={styles.blue} onClick={() => selectColorHandler('blue')}>Blue</button>
+    </section>
+  );
+}
+    
+ColorPicker.PropTypes = {
+  selectColorHandler: PropTypes.func.isRequired
+};
+
+export default ColorPicker;
